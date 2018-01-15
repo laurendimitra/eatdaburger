51fe08725e402ae96951fb11dd1512eedad2f4e3
laurendimitra <lauren.mooore@bluetruckstudios.co>
2018-01-15 00:24:05 -0500

    close

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Rest in Peas');
+INSERT INTO burgers (burger_name) VALUES ('Fig-eta Bout It');
+INSERT INTO burgers (burger_name) VALUES ('Olive And Let Die');
+INSERT INTO burgers (burger_name) VALUES ('Thank God Its Fried Egg');
+INSERT INTO burgers (burger_name) VALUES ('Ive Created a Muenster');
